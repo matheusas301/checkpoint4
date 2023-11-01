4a939a2efbc918a6932664259c4090b3364155f0
Rodrigo <rodrigogoncalves2005t@gmail.com>
2023-11-01 20:20:17 -0300

    Ajustes em erros do projeto

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import { Button } from "../button/page"
+import { Button } from "../button/button"
 import { Logo } from "../logo/Logo"
 import { ElementHeader, HeaderContainer } from "./Header.style"
 
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,4 +1,4 @@
-import { Footer } from "../footer/page"
+import { Footer } from "../footer/footer"
 import { Header } from "../header/Header"
 import { ElementMain } from "./Layout.style"
 
