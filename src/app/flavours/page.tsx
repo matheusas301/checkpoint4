0b1f1eada014d00308f23fee98dea7fc459a3b7b
Rodrigo <rodrigogoncalves2005t@gmail.com>
2023-11-01 20:12:13 -0300

    Mudança na organizacao das pastas

--- src/app/flavours/page.tsx
+++ src/app/flavours/page.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { Button } from "../../components/button/Button"
+import { Button } from "../../../components/button/Button"
 import { Layout } from "../../components/layout/Layout"
 import { routes } from "../../routes"
 import OrderContext from "../../contexts/OrderContext"
